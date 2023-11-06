2d35ab96ed5144f89ba2374ec516ca3fded546c2
逢跪跪 <610060008@qq.com>
2023-11-06 23:05:11 +0800

    feat:2.4.1

--- js/404.e1ee84fa.js
+++
@@ -1,1 +0,0 @@
-(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["404"],{cc89:function(n,t,c){"use strict";c.r(t);var e=function(){var n=this,t=n._self._c;return t("Exception",{attrs:{type:"404",imgColor:"",desc:"Oh~您的页面好像飞走了~"}})},o=[],s=c("2af9"),i={components:{Exception:s["i"]}},p=i,l=c("2877"),r=Object(l["a"])(p,e,o,!1,null,null,null);t["default"]=r.exports}}]);
